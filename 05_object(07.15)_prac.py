61fb1ffebff6fb0fbcb88a7d814cb7b53950a82c
Hee Joon Kang <kevin6353@naver.com>
2026-07-15 17:00:32 +0900

    실습문제

---
+++ 05_object(07.15)_prac.py
@@ -0,0 +1,56 @@
+# 실습1.
+
+count = 1
+temp = 1.0
+name = "희준"
+is_ok = True
+print(count, temp, name, is_ok)
+
+# 실습 2.
+print(type(count))
+print(type(temp))
+print(type(name))
+print(type(is_ok))
+
+
+# 실습 3.
+type(100)  # int
+type(100.0)  # float
+type("100")  # str
+
+print(type(100))
+print(type(100.0))
+print(type("100"))
+
+# 실습 4.
+print(1 + 1)
+print("1" + "2")
+print("10" + "10")
+
+
+# 실습 5.
+print(3 > 2)  # True
+print(5 == 5)  # True
+print(type(3 > 2))  # True
+
+
+# 실습 6.
+
+a = 10
+print(a, type(a))
+
+a = 10.0
+print(a, type(a))
+
+a = "10"
+print(a, type(a))
+
+
+# 실습 7.
+
+device_temp = 10.0
+check_count = 10
+device_name = "10"
+is_normal = True
+
+# type는 () 안에 , 를 통해 여러개 값을 연결시켜줄 수 없음
