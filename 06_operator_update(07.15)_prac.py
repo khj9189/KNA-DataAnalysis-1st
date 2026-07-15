21ca0959a0e47cc1c14c59aeb8bae91d98c1c88e
Hee Joon Kang <kevin6353@naver.com>
2026-07-15 17:49:15 +0900

    연산자 실습문제

---
+++ 06_operator_update(07.15)_prac.py
@@ -0,0 +1,22 @@
+# 실습1.
+
+a = 17
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a**b)
+
+# 실습2.
+
+a = 1
+b = 2
+c = 3
+
+print((a + b + c) / 3)
+print(c**2)
+print(a * b * c)
