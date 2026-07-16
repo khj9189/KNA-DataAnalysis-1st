dbb9831ded9baa63ad85babe7221d293e682ef3a
Hee Joon Kang <kevin6353@naver.com>
2026-07-16 09:39:12 +0900

    study: 비교연산자 활용

--- 06_operator_update(07.15).py
+++ 06_operator_update(07.15).py
@@ -87,3 +87,28 @@ print("정상" == "정상 ")  # False
 print("hello" != "hello")  # False (두 값이 동일한데 느낌표로 인해서 값이 반대로 출력됨)
 print("hello" != "hello ")  # True
 print("hello" != "Hello")  # True
+
+# 변수로 비교연산자 사용
+# "" 가 없다면 변수로 이해하고 위에서 변수에 할당 된 값을 찾는다.
+
+num1 = 123
+num2 = 456
+
+print(num1 >= num2)  # False
+# print(num1 >= "num2")
+# typeError : int와 str을 비교했기 때문이다.
+
+
+# 질문 1) hello 변수에 할당하는 값을 따옴표로 감싸지 않고 비교
+# print("=== 질문 1) hello 변수에 할당하는 값을 따옴표로 감싸지 않고 비교 ===")
+# hello = hi
+# print(hello == hi)  # NameError(선언하지 않은 이름 호출했을 때)
+# hi는 따옴표에 감싸져있지 않기 때문에 변수로 취급됨
+# 그런데 우리는 hi 변수를 선언한 적이 없기 때문에 에러
+
+# 질문 1) 해결방법
+# print("=== 질문 1) 해결 방법 ===")
+hi = "안녕"  # hello 변수에 hi 변수를 할당하기 전 hi 변수 선언
+hello = hi  # print(hello) > 안녕
+print("=== 변수 hello(안녕)와 변수 hi(안녕) 비교 ===")
+print(hello == hi)  # True
