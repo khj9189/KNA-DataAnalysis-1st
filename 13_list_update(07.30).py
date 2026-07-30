1d2e06bfb54c682c621cb8b95523039bc854032b
Hee Joon Kang <kevin6353@naver.com>
2026-07-30 17:48:15 +0900

    study:list_update

--- 13_list_update(07.30).py
+++ 13_list_update(07.30).py
@@ -24,4 +24,27 @@ for t in temps:
 
 print("high:", high)
 print("low:", low)
-print(low.sort())
+
+# 복습 sort(): 원본 배열을 오름차순으로 정렬해줌
+# 하지만 반환해주지 않기 때문에 print로 바로 찍으면 None 출력
+# 정렬된 배열을 출력하고 싶다면
+low.sort()
+print(low)
+
+
+# 리스트 안의 리스트
+# 표(행, 열) 처럼 한 줄에 여러 값이 묶인 데이터
+# 바깥 대괄호를 "행", 안쪽 인덱스 리스트를 "열"
+
+rows = [["펌프", 25], ["모터", 32], ["압축기", 28]]
+
+print(rows[0])
+print(type(rows[0]))  # 리스트를 출력
+
+# 중첩된 리스트 안의 값에 접근
+print(rows[1][1])
+# 중첩된 리스트 내부의 값은 대괄호를 여러번 이어서 접근
+
+# 리스트 안의 리스트 온도값만 출력하기
+for row in rows:
+    print(row[0], "온도", row[1])
