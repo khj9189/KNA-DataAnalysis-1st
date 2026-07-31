0e45f1dfa110e568c39534b11d0c66be1454b98c
Hee Joon Kang <kevin6353@naver.com>
2026-07-31 15:23:05 +0900

    study:tuple

--- 14_tuple(07.30).py
+++ 14_tuple(07.30).py
@@ -95,9 +95,88 @@ print("three:", three)
 # 튜플의 unpacking은 변수의 개수와 튜플에 담긴 값의 개수가 동일해야 함
 
 
-# 리스트 언패킹이 가능할까?
+# 리스트 언패킹이 가능할까? -> 가능하다.
 one, two, three, four = [11, 22, 33, 44]
 print("one:", one)
 print("two:", two)
 print("three:", three)
 print("four:", four)
+
+# ===============================================
+
+tup = (
+    "normal",
+    "normal",
+    "warning",
+    "normal",
+    "warning",
+)
+
+# 튜플의 길이 출력
+print(len(tup))  # 5
+
+# 특정 값의 갯수 세기기
+print(tup.count("warning"))  # 2 -> 찾고자 하는 값이 없는 경우 0이 나온다.
+
+# 특정 값이 처음 나온 인덱스
+print(tup.index("warning"))  # 012 번째 위치에 존재
+# print(tup.index("WArning")) -> value error: 찾고자 하는 값이 없는 경우 error가 발생한다.
+
+# 튜플 리스트
+# for 문으로 리스트를 사용해서 리스트 내부의 튜플에 접근하고 튜플에 담긴 값을 사용할 수 있음
+
+# 언패킹을 사용해서 접근한 튜플 내부의 값을 for문으로 변수에 바로 할당해서 접근한다.
+
+now = 0
+hour_13 = [("모터온도", 77), ("모터진동", 0.2), ("모터압력", 91)]
+for name, value in hour_13:
+    now += 1
+    print(now, "번째 반복")
+    print("name:", name, "value:", value)
+
+# ==================================================================
+
+temps_13 = [
+    ("qox_001", 81),
+    ("qox_002", 88),
+    ("qox_003", 95),
+    ("qox_004", 89),
+]
+
+warning = 90
+
+for name, temp in temps_13:
+    if temp >= warning:
+        print("경고", name, "설비 온도 이상")
+
+# 리스트 안의 튜플 갯수가 늘어나면 for문에서 변수를 여러 개 작성하면 된다.
+
+tup_list = [("일", "one", 1, "1"), ("이", "two", 2, "2")]
+
+# unpacking 할 때는 무조건 튜플의 값 갯수와 for문의 변수 갯수 통일
+# 통일하지 않을 경우 에러 발생한다.
+for kor_str, en_str, num, num_str in tup_list:
+    print("kor_str:", kor_str, "en_str:", en_str, "num:", num, "num_str:", num_str)
+
+# =====================================================================
+
+# 튜플 리스트 정렬
+# sorted()를 사용하여 튜플의 특정 값 기준으로 리스트를 정렬할 수 있음
+
+# temps_13을 정렬한다
+
+temps_13 = [
+    (81, "qox_001"),
+    (88, "qox_002"),
+    (95, "qox_003"),
+    (89, "qox_004"),
+]
+
+# sorted()는 원본 배열을 수정하지 않고 새 리스트를 반환해줌.
+hot = sorted(temps_13, reverse=True)
+print(hot)
+print("원본:", temps_13)
+
+# 원본이 바뀌게 하는 경우
+temps_13.sort(reverse=True)  # 원본 리스트 자체를 정렬 (반환값 없음, None)
+print(temps_13)  # 정렬된 상태로 바뀜
