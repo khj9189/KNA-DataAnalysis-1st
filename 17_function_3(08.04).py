07fb39ae4f8590ced40afa3f41b09c6389af89b4
Hee Joon Kang <kevin6353@naver.com>
2026-08-05 09:31:12 +0900

    study:function_3

--- 17_function_3(08.04).py
+++ 17_function_3(08.04).py
@@ -64,4 +64,44 @@ print("=== 실습2. ===")
 # 위에 코드 보기
 
 # ==========================================================
-print("=== 함수 설계와 조합 ===")
+print("=== 함수의 기본 예제 & 복습 ===")
+
+
+def say_hello():
+    pass  # 아무것도 안하는 코드
+
+
+def say_hi():
+    print("안녕하세요")
+
+
+# 함수는 선언된 후에 호출되어야 한다.
+say_hi()
+
+
+# 매개변수를 사용하면 더 다양한 일을 할 수 있습니다.
+def show_hello():
+    name = "NED"
+    print(f"안녕하세요, {name}")
+
+
+show_hello()
+
+
+# 매개변수는 여러 값을 받을 수 있고
+def show_hi(name, message):
+    # message = "안녕하세요"
+    print(f"{message}, {name}")
+
+
+show_hi("NED", "안녕하세요")
+
+
+# 매개변수에는 따로 안알려주면 기본값을 적용할 수도 있습니다.
+def show_greeting(name, message="안녕하세요"):
+    # message = "안녕하세요"
+    print(f"{message}, {name}")
+
+
+show_greeting("NED")
+show_greeting("jack", message="hello")
